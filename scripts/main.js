954196dfb7641f2d4b4aab81bf0e373e89704d33
Fidel Sanchez <me@fidel.engineer>
2019-10-31 21:23:02 -0400

    Added new menu image for when the menu expands

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,16 @@
  * JavaScript for emmet.vim tutorial
  */
 // Get menu button from DOM
-const menuButton = document.querySelector(".header-menu");
+const menuButton = document.querySelector("#header-menu");
 // Binding menu button onClick event to toggle navbar height
 menuButton.onclick = toggleMenuView;
 // navbar expanded className
 const classExpand = "expand";
+const classOpen = "open";
 
 function toggleMenuView(event) {
-  // toggles the height of the navbar
+  // toggles the height of the navbar and the image in the button
   let navbar = this.parentNode.parentNode;
   navbar.className = navbar.className === "" ? classExpand : "";
+  menuButton.className = menuButton.className === "" ? classOpen : "";
 }
